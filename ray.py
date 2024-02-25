4ccf86dd7dbc21bbd05074eb00386608dfea7021
rajaaditia <147203880+rajaaditia@users.noreply.github.com>
2024-02-25 11:55:00 +0700

    Add files via upload

---
+++ ray.py
@@ -0,0 +1,11 @@
+print ("=== aplikasi biodata ===")
+
+nama= input(" masukan nama: ")
+gender= input(" masukan gender:")
+tanggal_lahir= input(" masukan tanggal lahir: ")
+hobi= input(" masukan hobi: ")
+
+print ("nama :", nama)
+print("gender :", gender)
+print("tanggal lahir :", tanggal_lahir)
+print("hobi :", hobi)
